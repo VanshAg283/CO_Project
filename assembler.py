a528d0690c046a4c5128915707f7f96efabcfa6c
Vansh Aggarwal <vanshaggarwal2004@gmail.com>
2023-05-15 18:02:47 +0530

    Fixed errors

--- assembler.py
+++ assembler.py
@@ -41,7 +41,7 @@ for i in range(len(lines)):
             rest.append(lines[i])
         else:
             var_check = 0
-            errors.append("Typos in instruction name at line "+str(i+1)+"\n")
+            errors.append("Invalid operand at line "+str(i+1)+"\n")
             ct += 1
     else:
         ct += 1
@@ -50,29 +50,31 @@ for i in range(len(lines)):
 hlt_check = 0
 assembly = []
 inst = []
+ctins = 0
 for i in range(len(rest)):
     if rest[i][0] in A:
         if len(rest[i]) != 4:
-            errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append(rest[i][0]+" must contain 3 parameters at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][1] == "FLAGS" or rest[i][2] == "FLAGS" or rest[i][3] == "FLAGS":
-            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][1]) > 2 or rest[i][1] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][2]) > 2 or rest[i][2] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][3]) > 2 or rest[i][3] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         else:
             assembly.append(A[rest[i][0]]+"00"+reg[rest[i][1]]+reg[rest[i][2]]+reg[rest[i][3]])
             inst.append(rest[i])
+        ctins += 1
     elif rest[i][0] in B:
         if len(rest[i]) != 3:
-                errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append(rest[i][0]+" must contain 2 parameters at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][0] == "mov" and (rest[i][2] in reg or rest[i][2] == "FLAGS"):
             if rest[i][1] == "FLAGS":
-                errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+ctins+1)+"\n")
             elif len(rest[i][1]) > 2 or rest[i][1] not in reg:
-                errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
             else:
                 if rest[i][2] in reg:
                     assembly.append(C[rest[i][0]]+"00000"+reg[rest[i][1]]+reg[rest[i][2]])
@@ -82,41 +84,41 @@ for i in range(len(rest)):
                     inst.append(rest[i])
         else: 
             if rest[i][1] == "FLAGS":
-                errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+ctins+1)+"\n")
             elif len(rest[i][1]) > 2 or rest[i][1] not in reg:
-                errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
             elif not(rest[i][2][1:].isdigit) or not(rest[i][2].startswith("$")):
-                errors.append("Syntax error at line",ct+len(var)+i+1,"\n")
+                errors.append("Syntax error at line "+str(ct+len(var)+ctins+1)+"\n")
             elif int(rest[i][2][1:]) > 127 or int(rest[i][2][1:]) < 0:
-                errors.append("Illegal Immediate values at line "+str(ct+len(var)+i+1)+"\n")
+                errors.append("Illegal Immediate values at line "+str(ct+len(var)+ctins+1)+"\n")
             else:
                 assembly.append(B[rest[i][0]]+"0"+reg[rest[i][1]]+bin(int(rest[i][2][1:]))[2:].zfill(7))
                 inst.append(rest[i])
+        ctins += 1
     elif rest[i][0] in C:
         if len(rest[i]) != 3:
-            errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append(rest[i][0]+" must contain 2 parameters at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][1] == "FLAGS" or rest[i][2] == "FLAGS":
-            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][1]) > 2 or rest[i][1] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][2]) > 2 or rest[i][2] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         else:
             assembly.append(C[rest[i][0]]+"00000"+reg[rest[i][1]]+reg[rest[i][2]])
             inst.append(rest[i])
+        ctins += 1
     elif rest[i][0] in D:
         if len(rest[i]) != 3:
-            errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append(rest[i][0]+" must contain 2 parameters at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][1] == "FLAGS":
-            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Illegal use of FLAGS register at line "+str(ct+len(var)+ctins+1)+"\n")
         elif len(rest[i][1]) > 2 or rest[i][1] not in reg:
-            errors.append("Typo in register name at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Typo in register name at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][2] in lab:
-            errors.append("Misuse of labels as variables at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Misuse of labels as variables at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][2] not in var:
-            errors.append("Use of undefined variables at line "+str(ct+len(var)+i+1)+"\n")
-        elif rest[-1][0] not in F:
-            errors.append("hlt not being used as the last instruction\n")
+            errors.append("Use of undefined variable "+str(rest[i][2])+" at line "+str(ct+len(var)+ctins+1)+"\n")
         else:
             c = 0
             for j in range(len(rest)):
@@ -124,13 +126,14 @@ for i in range(len(rest)):
                     c += 1
             assembly.append(D[rest[i][0]]+"0"+reg[rest[i][1]]+bin(c+var.index(rest[i][2]))[2:].zfill(7))
             inst.append(rest[i])
+        ctins += 1
     elif rest[i][0] in E:
         if len(rest[i]) != 2:
-            errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append(rest[i][0]+" must contain 1 parameter at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][1] in var:
-            errors.append("Misuse of variables as labels at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Misuse of variables as labels at line "+str(ct+len(var)+ctins+1)+"\n")
         elif rest[i][1] not in lab:
-            errors.append("Use of undefined labels at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("Use of undefined label "+str(rest[i][1])+" at line "+str(ct+len(var)+ctins+1)+"\n")
         else:
             cwl = 0
             for j in range(len(rest)):
@@ -140,18 +143,21 @@ for i in range(len(rest)):
                     break
             assembly.append(E[rest[i][0]]+"0000"+bin(cwl)[2:].zfill(7))
             inst.append(rest[i])
+        ctins += 1
     elif rest[i][0][:-1] in lab:
         continue
     elif rest[i][0] in F:
         if len(rest[i]) != 1:
-            errors.append("Syntax error at line "+str(ct+len(var)+i+1)+"\n")
+            errors.append("hlt should not have any parameter at line "+str(ct+len(var)+ctins+1)+"\n")
         else:
             hlt_check = 1
             assembly.append(F[rest[i][0]]+"00000000000")
             inst.append(rest[i])
+        ctins += 1
+
 if hlt_check == 0:
     errors.append("Missing hlt instruction\n")
-if inst[-1][0] not in F:
+if inst[-1][0] not in F and "Missing hlt instruction\n" not in errors:
     errors.append("hlt not being used as the last instruction\n")
 
 if len(errors) == 0:
@@ -161,6 +167,7 @@ if len(errors) == 0:
             assembly_file.write(assembly[i]+"\n")
     with open ("errors.txt","w") as error_file:
         error_file.writelines(errors)
+    
 else:
     print("Errors found")
     with open ("errors.txt","w") as error_file:
